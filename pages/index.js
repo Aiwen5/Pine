7b74ffb904a9c40a501ee98bbfe1ba124053350d
Evan Schatz <eeschatz@icloud.com>
2024-04-21 01:57:18 -0700

    added chart logic, styled most of dish description page, and started the dish inventory array

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function LandingPage() {
       <main className={`${styles.main}`}>
         <div className={styles.container}>
           <h1 className={styles.title}>Welcome To</h1>
-          <Image src="/PineLogo.svg" alt="Pine Logo" width={300} height={300} />
+          <Image src="/PineLogo.svg" alt="Pine Logo" width={300} height={300} priority={true} />
           <div className={styles.buttons}>
           <Link href="/Home">
             <Button 
@@ -28,9 +28,11 @@ export default function LandingPage() {
               placeholder="Quiz Me"
             />
             </Link>
+            <Link href="/Description">
             <Button 
               placeholder="Tutorial"
             />
+            </Link>
           </div>
         </div>
       </main>
